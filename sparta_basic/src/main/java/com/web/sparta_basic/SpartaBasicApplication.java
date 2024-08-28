94aae15c3c99efcfaa2919cf3afb03bb416fc33b
developer1 <dev1@email.com>
2024-08-28 22:52:31 +0900

    first init

---
+++ sparta_basic/src/main/java/com/web/sparta_basic/SpartaBasicApplication.java
@@ -0,0 +1,13 @@
+package com.web.sparta_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaBasicApplication.class, args);
+    }
+
+}
---
+++ sparta_basic/src/test/java/com/web/sparta_basic/SpartaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.sparta_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
